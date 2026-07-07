15537223711361f02a3e5fa3c11617d516ed4d89
Saiko-developer <saikokayzin123@gmail.com>
2026-07-07 19:09:44 +0630

    data uploading

--- src/data/all-products/categories.js
+++ src/data/all-products/categories.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-// import localData from "../../../data.json";
+
 
 export const AllProductsCategoriesApiUrl = `${process.env.NEXT_PUBLIC_API_URL}/all-products-categories`;
 
@@ -12,9 +12,7 @@ export const fetchAllProducts = (...args) =>
   fetch(...args).then((res) => res.json());
 
 export const useProducts = create((set) => ({
-  // AllProductsCategoriesApiUrl:localData["all-products"],
-  // fetchAllProductsCategoriies
-  // AllProductsApiUrl:localData["all-products-categories"],
+
   category: "Galaxy Smartphone",
   setCategory: (category) => set({ category }),
   subname: "Galaxy S",
--- src/data/all-products/products.js
+++ src/data/all-products/products.js
@@ -1,16 +1,13 @@
 import { create } from "zustand";
-import localData from "../../../data.json";
+
 
 export const AllProductsApiUrl = `${process.env.NEXT_PUBLIC_API_URL}/all-products`;
 
 export const fetchAllProducts = (...args) =>
   fetch(...args).then((res) => res.json());
-// export const AllProductsApiUrl = "local-dummy-url-products";
-// export const fetchAllProducts = () => Promise.resolve(localData.products || []);
-// export const useProducts = () => localData.products || [];
-export const useProductStore = create((set) => ({
-//    AllProductsApiUrl: localData["all-products"] || [], 
 
+export const useProductStore = create((set) => ({
+  
   storage: "",
 
   setStorage: (storage) =>
